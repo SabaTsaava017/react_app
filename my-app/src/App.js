4dd77abf071413c758f4e398bd579bbed79d5835
SabaTsaava017 <sabacaava7@gmail.com>
2023-10-01 22:25:23 +0400

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,28 @@
+import input from "./input"
+
+
 function App() {
   return (
-    <div className="App">
-    <h1>saba</h1> 
+
+    <div className="container">
+
+        <div className="App">
+
+        <input type="text" className="input"  placeholder="User1" />
+        <input type="text" className="second_input" placeholder="User2" />
+
+
+        </div>
+
     </div>
+    
   );
 }
 
+
+
+
 export default App;
+
+
+
---
+++ my-app/src/input.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const input = ({type,width,height}) => {
+    return <input type={type} style={{width:width, height:height}}  />;
+
+
+};
+
+export default input;
